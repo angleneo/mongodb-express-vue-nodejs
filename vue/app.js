10beb508fca7ee9d41cc850946fb7bd386de7fd4
angleneo <ly564360784@qq.com>
2017-06-07 11:54:28 +0800

    改变了接口地址  可以把IP换成localhost就可以了

--- vue/app.js
+++ vue/app.js
@@ -5,28 +5,26 @@ var express = require('express');
 var Cookies = require('cookies'); var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-
-var router = require('router');
 var books = require('./routes/books');
 var Book = require('./schema/book');
 var Users = require('./schema/users');
 var app = express();
 app.use(bodyParser.json());
- app.use(function(req,res,next){
- 	req.cookies = new Cookies(req,res);
- 	req.userInfo={
+ // app.use(function(req,res,next){
+ // 	req.cookies = new Cookies(req,res);
+ // 	req.userInfo={
 
- 	};
- 	if(req.cookies.get('userInfo')){
- 		try{
- 			req.userInfo=JSON.parse(req.cookies.get('userInfo'));
+ // 	};
+ // 	if(req.cookies.get('userInfo')){
+ // 		try{
+ // 			req.userInfo=JSON.parse(req.cookies.get('userInfo'));
 
- 		}catch(e){
+ // 		}catch(e){
 
- 		}
- 	}
- 	next();
- })
+ // 		}
+ // 	}
+ // 	next();
+ // })
 // view engine setup
  // app.set('views', path.join(__dirname, 'views'));
  // app.set('view engine', 'jade');
@@ -41,26 +39,21 @@ app.use('/static', express.static('public'));
 
 
 
-mongoose.connect('mongodb://localhost:27011/bookstore');
+mongoose.connect('mongodb://localhost:27017/bookstore');
 // catch 404 and forward to error handler
-app.all('*',function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-  res.setHeader("P3P","CP=CAO PSA OUR");
-  if (req.method == 'OPTIONS') {
-    res.send(200); /让options请求快速返回/
-  }
-  else {
-    next();
-  }
-});
+// app.all('*',function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+//   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+//   res.setHeader("P3P","CP=CAO PSA OUR");
+//   if (req.method == 'OPTIONS') {
+//     res.send(200); /让options请求快速返回/
+//   }
+//   else {
+//     next();
+//   }
+// });
 var router = express.Router();
-
-router.use(function(req,res,next){
-	console.log('Something you do');
-	next();
-})
 
 
 
